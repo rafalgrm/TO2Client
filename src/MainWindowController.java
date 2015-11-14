e4d96692925660c6759790f22ad503b72ff917c8
mmajews <majewskimarcin@outlook.com>
2015-11-14 23:38:28 +0100

    Further development main window development

--- src/MainWindowController.java
+++ src/MainWindowController.java
@@ -1,3 +1,13 @@
+import javafx.fxml.FXML;
+import javafx.scene.canvas.Canvas;
+
 public class MainWindowController {
+    @FXML
+    private Canvas canvas;
+
+    public void initialize() {
+        System.out.println("Test");
+    }
 
 }
+
